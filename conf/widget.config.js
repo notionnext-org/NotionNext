f7671565d2e2f383350b2ad0e129242076d834a1
爱良 <130814915+Leafbritain@users.noreply.github.com>
2026-08-16 22:11:40 +0800

    Update widget.config.js

--- conf/widget.config.js
+++ conf/widget.config.js
@@ -28,6 +28,12 @@ module.exports = {
   MUSIC_PLAYER_AUDIO_LIST: [
     // 示例音乐列表。除了以下配置外，还可配置歌词，具体配置项看此文档 https://aplayer.js.org/#/zh-Hans/
     {
+      name: 'goladen hour',
+      artist: ' ',
+      url: 'http://music.163.com/song/media/outer/url?id=3406904185.mp3',
+      cover: 'https://i.postimg.cc/gcKQkbPT/IMG-8489.png'
+    },
+    {
       name: '朦胧之春',
       artist: 'sasakure.UK',
       url: 'http://music.163.com/song/media/outer/url?id=515337.mp3',
