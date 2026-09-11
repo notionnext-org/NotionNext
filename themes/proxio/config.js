060c9c81dc126e3b49412813fe21e8f34a0ec0b2
爱良 <130814915+Leafbritain@users.noreply.github.com>
2026-09-12 03:28:15 +0800

    Update config.js

--- themes/proxio/config.js
+++ themes/proxio/config.js
@@ -12,26 +12,26 @@ const CONFIG = {
   PROXIO_WELCOME_TEXT: '心爱的，再会了', // 欢迎文字，留空则不启用
 
   // 英雄区块导航
-  PROXIO_HERO_ENABLE: false, // 开启英雄区
-  PROXIO_HERO_TITLE_1: '开源且免费的基于 Notion 笔记的网站构建工具', // 英雄区文字
-  PROXIO_HERO_TITLE_2: '通过笔记无感知地建站、成倍放大您的价值', // 英雄区文字
+  PROXIO_HERO_ENABLE: true, // 开启英雄区
+  PROXIO_HERO_TITLE_1: ' ', // 英雄区文字
+  PROXIO_HERO_TITLE_2: ' ', // 英雄区文字
   // 英雄区两个按钮，如果TEXT留空则隐藏按钮
-  PROXIO_HERO_BUTTON_1_TEXT: '开始体验', // 英雄区按钮
+  PROXIO_HERO_BUTTON_1_TEXT: ' ', // 英雄区按钮
   PROXIO_HERO_BUTTON_1_URL:
-    'https://docs.tangly1024.com/article/vercel-deploy-notion-next', // 英雄区按钮
-  PROXIO_HERO_BUTTON_2_TEXT: '在Github上关注', // 英雄区按钮
-  PROXIO_HERO_BUTTON_2_URL: 'https://github.com/tangly1024/NotionNext', // 英雄区按钮
-  PROXIO_HERO_BUTTON_2_ICON: '/images/starter/github-mark.svg', // 英雄区按钮2的图标，不需要则留空
+    ' ', // 英雄区按钮
+  PROXIO_HERO_BUTTON_2_TEXT: ' ', // 英雄区按钮
+  PROXIO_HERO_BUTTON_2_URL: ' ', // 英雄区按钮
+  PROXIO_HERO_BUTTON_2_ICON: ' ', // 英雄区按钮2的图标，不需要则留空
 
-  // 英雄区配图，如需隐藏，改为空值即可 ''
-  PROXIO_HERO_BANNER_IMAGE: '', // hero区背景，默认是获取Notion背景，如需另外配置图片可以填写在这里
+  // 英雄区配图，如需隐藏，改为空值即可 'true'
+  PROXIO_HERO_BANNER_IMAGE: 'true', // hero区背景，默认是获取Notion背景，如需另外配置图片可以填写在这里
   PROXIO_HERO_BANNER_IFRAME_URL: '', // hero背景区内嵌背景网页 ，可以配置一个网页地址，例如动画网页https://my.spline.design/untitled-b0c6e886227646c34afc82cdc6de4ca2/
 
   // 文章区块
   PROXIO_BLOG_ENABLE: true, // 首页博文区块开关
-  PROXIO_BLOG_TITLE: '在庭院中',
-  PROXIO_BLOG_COUNT: 9, // 首页博文区块展示前4篇文章
-  PROXIO_BLOG_TEXT_1: '我的最新动态',
+  PROXIO_BLOG_TITLE: ' ',
+  PROXIO_BLOG_COUNT: 10, // 首页博文区块展示前4篇文章
+  PROXIO_BLOG_TEXT_1: '在庭院中',
   PROXIO_BLOG_AUTO_SHOW_COVER: false, // 首页博文区块是否默认显示文章主图；false 时保持悬浮显示主图的旧效果
 
   // 区块默认内容显示文章的summary文本，但也支持用自定义图片或logo德国替换掉占位显示内容
@@ -213,7 +213,7 @@ const CONFIG = {
   // 页脚菜单
   PROXIO_FOOTER_LINKS: [
     {
-      name: '我找到了你',
+      name: ' ',
       menus: [
         {
           title: '',
@@ -226,7 +226,7 @@ const CONFIG = {
       ]
     },
     {
-      name: '而你也找到了我',
+      name: '  ',
       menus: [
         { title: ' ', href: ' ' },
         {
