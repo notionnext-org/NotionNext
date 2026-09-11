594b871a58accae5852a14454d9ff99282290891
爱良 <130814915+Leafbritain@users.noreply.github.com>
2026-09-11 23:38:07 +0800

    Update config.js

--- themes/commerce/config.js
+++ themes/commerce/config.js
@@ -13,8 +13,8 @@ const CONFIG = {
     '心爱的，再会了', // 中间产品区块文字描述
   COMMERCE_TEXT_CENTER_CATEGORY_TITLE: '常春藤蔓', // 左侧产品分类标题
   COMMERCE_TEXT_FOOTER_TITLE: '', // COMMERCE主题页脚文案标题；按Shift+Enter键可以换行
-  COMMERCE_TEXT_FOOTER_MENU_1: '', // COMMERCE主题页脚左侧菜单标题1
-  COMMERCE_TEXT_FOOTER_MENU_2: '', // COMMERCE主题页脚左侧菜单标题2
+  COMMERCE_TEXT_FOOTER_MENU_1: '我找到了你', // COMMERCE主题页脚左侧菜单标题1
+  COMMERCE_TEXT_FOOTER_MENU_2: '而你也找到了我', // COMMERCE主题页脚左侧菜单标题2
 
   COMMERCE_FOOTER_RIGHT_IMG_URL: null, // 显示页脚右侧的图片，通常放二维码
   COMMERCE_FOOTER_RIGHT_TEXT: null, // 页脚右侧图片下的文字描述
